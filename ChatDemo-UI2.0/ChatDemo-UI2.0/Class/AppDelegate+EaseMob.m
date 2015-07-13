52d03d496a59a9058fc88715e0d831b002009028
dujiepeng <347302029@qq.com>
2015-07-13 17:51:28 +0800

    update

--- ChatDemo-UI2.0/ChatDemo-UI2.0/Class/AppDelegate+EaseMob.m
+++ ChatDemo-UI2.0/ChatDemo-UI2.0/Class/AppDelegate+EaseMob.m
@@ -225,6 +225,9 @@
     else{
         alertView = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"prompt", @"Prompt") message:NSLocalizedString(@"login.beginAutoLogin", @"Start automatic login...") delegate:nil cancelButtonTitle:NSLocalizedString(@"ok", @"OK") otherButtonTitles:nil, nil];
         
+        
+        // 旧数据转换 (如果您的sdk是由2.1.2版本升级过来的，需要家这句话)
+        [[EaseMob sharedInstance].chatManager importDataToNewDatabase];
         //获取数据库中的数据
         [[EaseMob sharedInstance].chatManager loadDataFromDatabase];
     }
